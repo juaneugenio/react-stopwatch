c8304d40e7d9ecac1b754906df43d238659441f3
juaneugenio <leuviah77gmail.com>
2024-08-25 10:50:31 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+/** @format */
+
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+	const [time, setTime] = useState(0);
+	const [running, setRunning] = useState(false);
+
+	useEffect(() => {
+		let interval;
+		if (running) {
+			interval = setInterval(() => setTime((prevTime) => prevTime + 10), 10);
+		} else if (!running) {
+			clearInterval(interval);
+		}
+		return () => clearInterval(interval);
+	}, [running]);
+	return (
+		<>
+			<h1>STOP WATCH</h1>
+			<div>
+				<span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+				<span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+				<span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+			</div>
+			<div>
+				{running ? (
+					<button onClick={() => setRunning(false)}>Stop</button>
+				) : (
+					<button onClick={() => setRunning(true)}>Start</button>
+				)}
+
+				<button onClick={() => setTime(0)}>RESET</button>
+			</div>
+		</>
+	);
+}
+
+export default App;
